9a734a71244bc5a84ef510dbf4e472e851a28fde
manolis-kapetanakis-cko <manolis.kapetanakis@checkout.com>
2021-03-02 16:57:33 +0000

    remove css from sdk

--- projects/manos-afrm/src/lib/cvv.component.ts
+++ projects/manos-afrm/src/lib/cvv.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'cvv',
-  template: `<div class="cvv-frame"></div>`,
-  styleUrls: ['./global/global.css']
+  template: `<div class="cvv-frame"></div>`
 })
 export class CvvComponent implements OnInit {
 
--- projects/manos-afrm/src/lib/expiry-date.component.ts
+++ projects/manos-afrm/src/lib/expiry-date.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'expiry-date',
-  template: `<div class="expiry-date-frame"></div>`,
-  styleUrls: ['./global/global.css']
+  template: `<div class="expiry-date-frame"></div>`
 })
 export class ExpiryDateComponent implements OnInit {
 
